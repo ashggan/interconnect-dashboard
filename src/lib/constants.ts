6ded3d19a5b64173ac1d119d3e37ab6429583295
ashggan <ashganwiki@gmail.com>
2025-05-01 20:18:51 +0200

    feat: set baseURl

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,11 @@
+// const isDev = process.env.NODE_ENV === 'development';
+// export const baseUrl = isDev
+//   ? process.env.BASE_URL
+//   : process.env.NEXT_PUBLIC_BASE_URL || 'https://your-production-url.com';
+
 const isDev = process.env.NODE_ENV === 'development';
 export const baseUrl = isDev
-  ? process.env.BASE_URL
-  : process.env.NEXT_PUBLIC_BASE_URL || 'https://your-production-url.com';
+  ? 'http://localhost:3000' // Local development URL
+  : process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` // Vercel provides this environment variable
+    : ''; // Fallback empty string
