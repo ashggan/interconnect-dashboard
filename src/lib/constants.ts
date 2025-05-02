deabbc555e2eaa1d5360c8d9f62820295f39fc5a
ashggan <ashganwiki@gmail.com>
2025-05-02 02:25:48 +0200

    chore: rebuild

---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+// const isDev = process.env.NODE_ENV === 'development';
+export const baseUrl = 'https://interconnect-dashboard.vercel.app';
+
+// VERCEL_URL=https://interconnect-dashboard.vercel.app
+// NEXT_PUBLIC_VERCEL_URL=localhost:3000
