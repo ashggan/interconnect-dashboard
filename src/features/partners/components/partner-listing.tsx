d372ac49cb3d32c18ef168141add644984302071
ashggan <ashganwiki@gmail.com>
2025-05-03 12:46:36 +0200

    feat: change url in partner listing

--- src/features/partners/components/partner-listing.tsx
+++ src/features/partners/components/partner-listing.tsx
@@ -2,6 +2,7 @@ import { Partner } from '@/constants/data';
 // import { searchParamsCache } from '@/lib/searchparams';
 import { DataTable as PartnerTable } from '@/components/ui/table/data-table';
 import { columns } from './partner-tables/columns';
+import { baseUrl } from '@/lib/constants';
 
 type PartnerListingPage = {};
 
@@ -19,9 +20,7 @@ export default async function PartnerListingPage({}: PartnerListingPage) {
   //   ...(categories && { categories: categories })
   // };
 
-  const response = await fetch(
-    `https://interconnect-dashboard.vercel.app/api/partner`
-  );
+  const response = await fetch(`${baseUrl}/api/partner`);
   const data = await response.json();
   const totalPartners = data.partners?.length || 0;
   const partners: Partner[] = data.partners || [];
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,1 @@
-// const isDev = process.env.NODE_ENV === 'development';
 export const baseUrl = 'https://interconnect-dashboard.vercel.app';
-
-// VERCEL_URL=https://interconnect-dashboard.vercel.app
-// NEXT_PUBLIC_VERCEL_URL=localhost:3000
